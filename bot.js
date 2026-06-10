5e72f3e6a36d4803b3c3ec6e5bf6a5ee73fd9766
6amigos6 <qaqascell66@gmail.com>
2026-06-10 14:12:58 +0400

    Change bot token and reset domain
    Updated bot token and cleared domain.

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
 module.exports = {
-  token:  "8286423335:AAH1f5I4NM7B5nmJtEL7i-hCt5Umms7Aj_8",
-  domain: "https://9252c32bea9b84.lhr.life"
+  token:  "8600229881:AAEK5YDBIyKXAwIf5HhIwtEBr9xwUpqJXJY",
+  domain: ""
 };
